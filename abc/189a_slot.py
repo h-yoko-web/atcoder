c082eef3a512b79a5433999b03a98e2447614682
h-yoko-web <yokohaya1027@gmail.com>
2021-02-02 14:06:26 +0900

    abc189a

---
+++ abc/189a_slot.py
@@ -0,0 +1,9 @@
+s = input()
+print('Won') if s[0] == s[1] and s[1] == s[2] else print('Lost')
+
+# s = input()
+# c = []
+# for i in s:
+#     c.append(i)
+
+# print('Won') if c[0] == c[1] == c[2] else print('Lost')
